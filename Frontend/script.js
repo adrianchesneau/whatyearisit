e94d662e79871741e0a451dc3f55f270dd8252ab
adrian <adrian.chesneau@gmail.com>
2024-01-26 11:28:10 +0100

    oui oui allez

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-backend-one-omega.vercel.app/year')
+fetch('https://whatyearisit-backend-one-omega.vercel.app/date')
     .then(response => response.json())
     .then(date => {
         document.querySelector('#year').textContent = date.year
